62ed2541d8928a44371bbcbd81762622e737c85b
simskij <simon.aronsson@outlook.com>
2016-08-04 09:33:32 +0200

    Reverting parts of a previous commit.
    Revertering parts of 18b3c025abe410fd436ab65b23a35ff1fba299d1 as it
    introduces breaking changes to the solution. Hale.Lib needs to remain
    in source control tracking as its essential to get the solution to build.
    :)

---
+++ Shared/Hale.Lib/Modules/Exceptions/UnknownUnsuccessfulException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hale.Lib.Modules.Exceptions
+{
+    class UnknownUnsuccessfulException: Exception
+    {
+        public UnknownUnsuccessfulException(): 
+            base("The function did not run successfully, but no exception was provided.")
+        {
+           
+        }
+    }
+}
