c1d7b0c8001900e757266ddba91a818155d44a56
simon aronsson <simon@io.nav>
2016-10-12 19:24:25 +0200

    Migration from gulp to webpack.

--- Frontend/Web/src/directives/widgets/widget-toggler/widget-toggler-controller.js
+++
@@ -1,8 +0,0 @@
-(function() {
-  'use strict';
-
-  angular.module('hale.gui')
-  .controller('WidgetTogglerController', ['$scope', 'Widgets', function($scope, Widgets) {
-    $scope.widgets = Widgets.List();
-  }])
-})();
