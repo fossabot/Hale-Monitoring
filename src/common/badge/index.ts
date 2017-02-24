bbde68314c156c890e2df500a48c90e628e82e37
simskij <simon.aronsson@outlook.com>
2017-02-24 20:50:39 +0100

    make clicking badge sign out the user

--- src/common/badge/index.ts
+++ src/common/badge/index.ts
@@ -1,6 +1,7 @@
 import { IComponentOptions } from 'angular';
 
 const template = require('./badge.html');
+require('./badge.scss');
 
 export class BadgeComponent implements IComponentOptions {
   templateUrl: any;
@@ -8,10 +9,26 @@ export class BadgeComponent implements IComponentOptions {
 
   constructor() {
     this.templateUrl = template;
-    this.controller = BadgeController;
+    this.controller = [
+      'Users',
+      BadgeController
+    ];
   }
 }
 
 export class BadgeController {
+  user: any;
 
+  constructor(private Users: any) {}
+
+  $onInit() {
+    this.getUser();
+  }
+  private getUser(): void {
+    this.Users
+      .getCurrent()
+      .then((user: any) => {
+        this.user = user;
+      });
+  }
 }
