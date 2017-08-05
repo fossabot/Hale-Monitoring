ece3da61165ab974fd5ee1fded0d46cfd50aa79e
nils måsén <nils@piksel.se>
2017-08-05 19:54:03 +0200

    Refactor agent config to use a raw serializable object

--- Shared/Hale.Lib/Config/AgentConfig.cs
+++ Shared/Hale.Lib/Config/AgentConfig.cs
@@ -15,69 +15,75 @@ using Hale.Lib.Modules;
 
 namespace Hale.Lib.Config
 {
-    public class AgentConfig
+    public abstract class AgentConfigBase
     {
-
-        [YamlMember( Alias ="checks")]
-        public Dictionary<string, Dictionary<string, object>> _checks { get; set; }
-            = new Dictionary<string, Dictionary<string, object>>();
-
-        [YamlIgnore]
-        public Dictionary<string, CheckSettings> Checks { get; set; }
-
-        [YamlMember(Alias = "info")]
-        public Dictionary<string, Dictionary<string, object>> _info { get; set; }
-            = new Dictionary<string, Dictionary<string, object>>();
-
-        [YamlIgnore]
-        public Dictionary<string, InfoSettings> Info { get; set; }
-
-        [YamlMember(Alias = "actions")]
-        public Dictionary<string, Dictionary<string, object>> _actions { get; set; }
-            = new Dictionary<string, Dictionary<string, object>>();
-
-        [YamlIgnore]
-        public Dictionary<string, ActionSettings> Actions { get; set; }
-
         public Dictionary<string, AgentConfigModule> Modules { get; set; }
         public Dictionary<string, AgentConfigTask> Tasks { get; set; } = new Dictionary<string, AgentConfigTask>();
+    }
+
+    class AgentConfigRaw: AgentConfigBase
+    {
+        public Dictionary<string, Dictionary<string, object>> Checks { get; set; }
+            = new Dictionary<string, Dictionary<string, object>>();
+
+        public Dictionary<string, Dictionary<string, object>> Info { get; set; }
+            = new Dictionary<string, Dictionary<string, object>>();
+
+        public Dictionary<string, Dictionary<string, object>> Actions { get; set; }
+            = new Dictionary<string, Dictionary<string, object>>();
+    }
+
+    public class AgentConfig: AgentConfigBase
+    {
+        public Dictionary<string, CheckSettings> Checks { get; set; }
+
+        public Dictionary<string, InfoSettings> Info { get; set; }
+
+        public Dictionary<string, ActionSettings> Actions { get; set; }
+
+
+
+
+        /// <summary>
+        /// Returns a new empty instance of AgentConfig with all fields initialized
+        /// </summary>
+        public static AgentConfig Empty => new AgentConfig().InitializeAgentConfigLists();
 
         static readonly NumberFormatInfo nfi = new NumberFormatInfo() { NumberDecimalSeparator = "." };
 
         public static AgentConfig LoadFromFile(string file)
         {
-            AgentConfig ac;
+            AgentConfigRaw acr;
 
             using (StreamReader sr = File.OpenText(file))
             {
                 Deserializer ds = new Deserializer(namingConvention: new CamelCaseNamingConvention());
-                ac = ds.Deserialize<AgentConfig>(sr);
+                acr = ds.Deserialize<AgentConfigRaw>(sr);
             }
 
-            CheckAgentConfigForNull(ac);
-            ac = InitializeAgentConfigLists(ac);
+            CheckAgentConfigForNull(acr);
 
+            var ac = AgentConfig.Empty;
             ac = SetDefaultTaskValues(ac);
 
-            if (ac._checks != null)
-                ac = LoadSerializedCheck(ac);
+            if (acr.Checks != null)
+                ac.LoadSerializedCheck(acr);
 
-            if (ac._info != null)
-                ac = LoadSerializedInfo(ac);
+            if (acr.Info != null)
+                ac.LoadSerializedInfo(acr);
 
-            if (ac._actions != null)
-                ac = LoadSerializedActions(ac);
+            if (acr.Actions != null)
+                ac.LoadSerializedActions(acr);
 
             return ac;
         }
 
-        private static AgentConfig InitializeAgentConfigLists(AgentConfig ac)
+        private AgentConfig InitializeAgentConfigLists()
         {
-            ac.Checks = new Dictionary<string, CheckSettings>();
-            ac.Info = new Dictionary<string, InfoSettings>();
-            ac.Actions = new Dictionary<string, ActionSettings>();
-
-            return ac;
+            Checks = new Dictionary<string, CheckSettings>();
+            Info = new Dictionary<string, InfoSettings>();
+            Actions = new Dictionary<string, ActionSettings>();
+            return this;
         }
 
         private static AgentConfig SetDefaultTaskValues(AgentConfig ac)
@@ -115,61 +121,59 @@ namespace Hale.Lib.Config
             return ac;
         }
 
-        private static AgentConfig LoadSerializedActions(AgentConfig ac)
+        private void LoadSerializedActions(AgentConfigRaw acr)
         {
-            foreach (var _action in ac._actions)
+            foreach (var action in acr.Actions)
             {
-                var actionProperties = GetProperties(_action.Value);
+                var actionProperties = GetProperties(action.Value);
                 var actionSettings = new ActionSettings(actionProperties);
                 actionSettings.TargetSettings = new Dictionary<string, Dictionary<string, string>>();
                 var targetSettings = new Dictionary<string, string>();
-                foreach (var target in GetTargets(_action.Value))
+                foreach (var target in GetTargets(action.Value))
                     actionSettings.TargetSettings.Add(target.Key, targetSettings);
                 actionSettings.ParseRaw();
-                ac.Actions.Add(_action.Key, actionSettings);
-            }
 
-            return ac;
+                Actions.Add(action.Key, actionSettings);
+            }
         }
 
-        private static AgentConfig LoadSerializedInfo(AgentConfig ac)
+        private void LoadSerializedInfo(AgentConfigRaw acr)
         {
-            foreach (var _info in ac._info)
+            foreach (var info in acr.Info)
             {
-                var infoProperties = GetProperties(_info.Value);
+                var infoProperties = GetProperties(info.Value);
                 var infoSettings = new InfoSettings(infoProperties);
                 infoSettings.TargetSettings = new Dictionary<string, Dictionary<string, string>>();
                 var targetSettings = new Dictionary<string, string>();
-                foreach (var target in GetTargets(_info.Value))
+                foreach (var target in GetTargets(info.Value))
                     infoSettings.TargetSettings.Add(target.Key, targetSettings);
                 infoSettings.ParseRaw();
-                ac.Info.Add(_info.Key, infoSettings);
-            }
 
-            return ac;
+                Info.Add(info.Key, infoSettings);
+            }
         }
 
-        private static AgentConfig LoadSerializedCheck(AgentConfig ac)
+        private void LoadSerializedCheck(AgentConfigRaw acr)
         {
-            foreach (var _check in ac._checks)
+            foreach (var check in acr.Checks)
             {
-                var checkProperties = GetProperties(_check.Value);
+                var checkProperties = GetProperties(check.Value);
                 var checkSettings = new CheckSettings(checkProperties);
                 checkSettings.TargetSettings = new Dictionary<string, Dictionary<string, string>>();
-                checkSettings.Actions = GetActions(_check.Value);
-                checkSettings.Thresholds = GetThresholds(_check.Value);
+                checkSettings.Actions = GetActions(check.Value);
+                checkSettings.Thresholds = GetThresholds(check.Value);
                 var targetSettings = new Dictionary<string, string>();
-                foreach (var target in GetTargets(_check.Value))
+                foreach (var target in GetTargets(check.Value))
                     checkSettings.TargetSettings.Add(target.Key, targetSettings);
                 checkSettings.ParseRaw();
-                ac.Checks.Add(_check.Key, checkSettings);
+
+                Checks.Add(check.Key, checkSettings);
             }
-            return ac;
         }
 
-        private static void CheckAgentConfigForNull(AgentConfig ac)
+        private static void CheckAgentConfigForNull(AgentConfigRaw acr)
         {
-            if (ac == null)
+            if (acr == null)
             {
                 throw new Exception("Could not deserialize config.yaml. Make sure the syntax is correct.");
             }
