f42e51b494dca3cd54c0da96515ceebc6e5848e2
nils måsén <nils@piksel.se>
2017-08-05 09:42:33 +0200

    Bump versions

--- Modules/Hale.Check.MemoryUsage/Properties/AssemblyInfo.cs
+++ Modules/Hale.Check.MemoryUsage/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.2.16227.5")]
-[assembly: AssemblyFileVersion("1.2.16227.5")]
+[assembly: AssemblyVersion("1.2.17204.6")]
+[assembly: AssemblyFileVersion("1.2.17204.6")]
