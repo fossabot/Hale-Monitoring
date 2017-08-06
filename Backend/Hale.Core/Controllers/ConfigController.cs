8c6e88205c6c46ac1817304adb0e55e3910d5e69
simskij <simon.aronsson@outlook.com>
2017-08-06 16:33:10 +0200

    add dtos for summaries

--- Backend/Hale.Core/Controllers/ConfigController.cs
+++ Backend/Hale.Core/Controllers/ConfigController.cs
@@ -1,7 +1,6 @@
 ﻿using Hale.Core.Contexts;
 using Hale.Core.Models.Agent;
 using NLog;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,12 +9,24 @@ using System.Web.Http;
 using System.Web.Http.Description;
 using System.Data.Entity;
 using Hale.Core.Utils;
+using Hale.Lib.Modules;
+using Hale.Core.Models;
+using System.Linq.Expressions;
+using System;
+using Hale.Core.Models.Users;
 
 namespace Hale.Core.Controllers
 {
     [RoutePrefix("api/v1/configs")]
     public class ConfigController : ApiController
     {
+
+        private Expression<Func<Account, UserSummaryDTO>> CreateUserSummaryDTO = account => new UserSummaryDTO
+        {
+            Id = account.Id,
+            FullName = account.FullName,
+            Email = account.Email,
+        };
 
         #region Constructors and declarations
         private readonly ILogger _log;
@@ -29,18 +40,35 @@ namespace Hale.Core.Controllers
         }
         #endregion
 
+
         //[Authorize]
         [Route()]
         [ResponseType(typeof(List<AgentConfigSet>))]
         [AcceptVerbs("GET")]
         public IHttpActionResult List()
         {
+
+
+
             var configList = _db.AgentConfigs
-                .Include( c => c.Functions.Select( f => f.Module ) )
+                .Include( c => c.Functions )
                 .Include( c => c.Tasks )
-                .ToList();
+                .Select(c => new ConfigSummaryDTO
+                {
+                    Id = c.Id,
+                    Identifier = c.Identifier,
+                    CheckCount = c.Functions.Where(x => x.Type == ModuleFunctionType.Check).Count(),
+                    ActionCount = c.Functions.Where(x => x.Type == ModuleFunctionType.Action).Count(),
+                    InfoCount = c.Functions.Where(x => x.Type == ModuleFunctionType.Info).Count(),
+                    Modified = c.Modified,
+                    ModifiedBy = _db.Accounts.Where(a => a.Id == c.ModifiedBy).Select(CreateUserSummaryDTO).FirstOrDefault(),
+                    Created = c.Created,
+                    CreatedBy = _db.Accounts.Where(a => a.Id == c.CreatedBy).Select(CreateUserSummaryDTO).FirstOrDefault(),
+                }).ToList();
             return Ok(configList);
         }
+
+
 
         //[Authorize]
         [Route("{id}")]
