f0f96f13cdb23bddf1727662ac4c8d3494ddd56c
nils måsén <nils@piksel.se>
2018-07-29 22:23:51 +0200

    Add ServiceBase facade for non-windows use

---
+++ Shared/Hale.Lib/Facades/ServiceBase.cs
@@ -0,0 +1,28 @@
+﻿#if NETSTANDARD
+using System;
+
+namespace Hale.Lib.Facades
+{
+    public class ServiceBase: IDisposable
+    {
+        public string ServiceName { get; set; }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        protected virtual void OnStart(string[] args)
+        {
+        }
+
+        protected virtual void OnStop()
+        {
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+        }
+    }
+}
+#endif
